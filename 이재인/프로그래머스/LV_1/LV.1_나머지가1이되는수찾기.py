5ff130d09c44134f7f2dcb44c5f03c57d4df55e3
Leewodls <wodls2436@gmail.com>
2025-03-26 16:09:55 +0900

    프로그래머스_LV.1_나머지가1이되는수찾기

---
+++ 이재인/프로그래머스/LV_1/LV.1_나머지가1이되는수찾기.py
@@ -0,0 +1,10 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def solution(n):
+...     answer=1
+...     while 1:
+...         if n % answer ==1:
+...             break
+...         answer+=1
+...     return answer
+... 
