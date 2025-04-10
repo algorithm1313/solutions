cde4de62283b0ea17f3e68bf0ca06855c0eae0f9
kosssshhhh <kosukwhon@gmail.com>
2025-04-10 13:00:25 +0900

    프로그래머스 타겟 넘버 js 풀이

---
+++ 고석환/프로그래머스/LV2_타겟_넘버/solution.js
@@ -0,0 +1,9 @@
+const go = (idx, v, target, numbers) => {
+    if(idx === numbers.length) return v === target ? 1 : 0;
+    
+    return go(idx + 1, v + numbers[idx], target, numbers) + go(idx + 1, v - numbers[idx], target, numbers);
+}
+
+function solution(numbers, target) {
+    return go(0, 0, target, numbers);
+}
