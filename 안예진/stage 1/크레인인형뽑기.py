fd0d9172f51ce12b19cc69a72916fdad887a066a
ayj0808 <ayj0808@naver.com>
2025-04-29 00:50:01 +0900

    ayj/올바른괄호

---
+++ 안예진/stage 1/크레인인형뽑기.py
@@ -0,0 +1,19 @@
+# def solution(board, moves):
+#     a = len(board)
+#     basket = []
+#     popx=0 #쌓여서 터진 인형 0으로 초기화
+    
+#     for i in range(a):
+#         doll = board[i][?]
+#         if doll ==0:
+#             continue
+            
+#         board[][] = 0 
+#         #바구니 검사
+#         if basket and basket[-1] ==doll:
+#             basket.pop()
+#             popx +=2
+#         else:
+#             basket.append(doll)
+#         break
+#     return popx
